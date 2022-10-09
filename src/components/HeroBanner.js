199b3cbd08c19a69d9e37b2e0d36565d57945b5b
Derrick <derrickryan10@gmail.com>
2022-10-09 11:01:33 -0400

    Edit banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -6,7 +6,7 @@ import HeroBannerImage from '../assets/images/banner.png';
 const HeroBanner = () => {
   return (
     <Box sx={{ mt: { lg: '50px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-      <Typography color="#FF2625" variant="h4" fontWeight="600">Exercise Explorer</Typography>
+      <Typography color="#FF2625" fontSize="36px" fontWeight="600">Exercise Explorer</Typography>
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
         Browse and Search
         <br />Exercises
@@ -16,7 +16,7 @@ const HeroBanner = () => {
         <br />with included descriptions and videos
       </Typography>
       <Button variant="contained" href="#exercises" sx={{ backgroundColor: "#FF2625" }}>Explore</Button>
-      <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '160px' }}>
+      <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '140px' }}>
         Exercise
       </Typography>
       <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import Logo from '../assets/images/logo-2.jpeg';
 
 const Navbar = () => {
   return (
-    <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+    <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '20px', xs: '20px' }, justifyContent: 'none' }} px="20px">
       <Link to="/">
         <img src={Logo} alt="logo" style={{ width: '140px', height: '140px', margin: '0px 20px' }} />
       </Link>
