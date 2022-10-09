465413420e272951dd04c17742fc676e4e866d23
Derrick <derrickryan10@gmail.com>
2022-10-09 15:26:48 -0400

    Edit homepage

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Box, Typography, Button } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+import HeroBannerImage from '../assets/images/weights.jpeg'
 
 const HeroBanner = () => {
   return (
     <Box sx={{ mt: { lg: '50px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-      <Typography color="#FF2625" fontSize="36px" fontWeight="600">Exercise Explorer</Typography>
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      <Typography color="#FF2625" fontSize="50px" fontWeight="700">Exercise Explorer</Typography>
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '28px', xs: '40px' } }} mb="23px" mt="30px">
         Browse and Search
         <br />Exercises
       </Typography>
@@ -17,7 +17,7 @@ const HeroBanner = () => {
       </Typography>
       <Button variant="contained" href="#exercises" sx={{ backgroundColor: "#FF2625" }}>Explore</Button>
       <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '140px' }}>
-        Exercise
+        Exercises
       </Typography>
       <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
     </Box>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
-import Logo from '../assets/images/logo-2.jpeg';
+import Logo from '../assets/images/logo-5.png';
 
 const Navbar = () => {
   return (
     <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '20px', xs: '20px' }, justifyContent: 'none' }} px="20px">
       <Link to="/">
-        <img src={Logo} alt="logo" style={{ width: '140px', height: '140px', margin: '0px 20px' }} />
+        <img src={Logo} alt="logo" style={{ width: '128px', height: '128px', margin: '0px 20px' }} />
       </Link>
       <Stack
         direction="row"
